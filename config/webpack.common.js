68ec74ab1834c07f5e45fc3fe39f72f9ef5e790b
Jason Watmore <jason@pointblankdevelopment.com.au>
2017-05-09 12:54:48 +1000

    Updated to Angular 4.1.1

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,41 +11,54 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
 
     module: {
-        loaders: [
+        rules: [
           {
               test: /\.ts$/,
-              loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+              loaders: [
+                {
+                    loader: 'awesome-typescript-loader',
+                    options: { configFileName: helpers.root('src', 'tsconfig.json') }
+                }, 'angular2-template-loader'
+              ]
           },
           {
               test: /\.html$/,
-              loader: 'html'
+              loader: 'html-loader'
           },
           {
               test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-              loader: 'file?name=assets/[name].[hash].[ext]'
+              loader: 'file-loader?name=assets/[name].[hash].[ext]'
           },
           {
               test: /\.css$/,
               exclude: helpers.root('src', 'app'),
-              loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+              loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
           },
           {
               test: /\.css$/,
               include: helpers.root('src', 'app'),
-              loader: 'raw'
+              loader: 'raw-loader'
           },
           {
               test: /\.less$/,
-              loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+              loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!less-loader' })
           }
         ]
     },
 
     plugins: [
+      // Workaround for angular/angular#11580
+      new webpack.ContextReplacementPlugin(
+        // The (\\|\/) piece accounts for path separators in *nix and Windows
+        /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+        helpers.root('./src'), // location of your src
+        {} // a map of your routes
+      ),
+
       new webpack.optimize.CommonsChunkPlugin({
           name: ['app', 'vendor', 'polyfills']
       }),
@@ -56,6 +69,6 @@ module.exports = {
 
       new webpack.ProvidePlugin({
           $: 'jquery',
-      }),
+      })
     ]
 };
